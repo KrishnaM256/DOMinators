4fc3278c572edb5ad448d3ae50083c9ca07dfeb9
Krishna Magar <magarkrishna2564@gmail.com>
2025-01-25 08:33:25 +0530

    .

--- frontend/src/redux/constants.js
+++ frontend/src/redux/constants.js
@@ -1,4 +1,4 @@
 export const BASE_URL = 'http://localhost:5000'
 export const USERS_URL = '/api/users'
-export const FRONT_URL = 'http://localhost:5173'
+export const FRONT_URL = 'http://localhost:5174'
 export const POSTS_URL = '/api/posts'
